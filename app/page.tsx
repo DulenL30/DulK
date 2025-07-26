90c5ecb15b1fa8dadfe665b44880f00a1fcd427a
DulenL30 <dulenvinjaya21@gmail.com>
2025-07-26 13:02:59 +0530

    Fix sum errors in sheme.prisma and save user to the database.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Homepage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline p-4">Dulk</h1>
+      <h1 className="text-3xl font-bold underline p-4 text-center">Dulk</h1>
     </div>
   );
 }
